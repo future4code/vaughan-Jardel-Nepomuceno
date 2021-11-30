2f539100114fd3504760240c57222c80b67ac114
jardel <jardelsantos15@gmail.com>
2021-11-30 17:14:55 -0300

    exercicio feito

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,86 @@
+/*exercicio 1 
+
+a.  false , b.  false, c.  true , d.  boolean */
+
+// exercicio 2
+
+// adicionei um Number no prompt pois o código antigo estava somando duas strings em vez de dois números.
+
+// exercicio 3
+
+// let primeiroNumero = Number(prompt ("digite um número"))
+
+// let segundoNumero = Number(prompt ("digite um número"))
+
+// let soma = primeiroNumero + segundoNumero
+
+// console.log (soma) 
+
+// Exercícios de escrita de código
+
+// exercicio 1
+
+// const idadePessoa1 = Number(prompt("Qual sua idade?"))
+
+// const idadePessoa2 = Number(prompt("Qual idade do seu amigo?"))
+
+// const resultado = idadePessoa1 > idadePessoa2
+
+// console.log ("Sua idade é maior do que a do seu melhor amigo?" , resultado)
+
+// let subtracao = idadePessoa1 - idadePessoa2
+
+// console.log(subtracao);
+
+// exercicio 2
+
+// const numeroPar = Number(prompt("Digite um número par")) 
+
+// const resultado = numeroPar % 2
+
+// console.log(resultado)
+
+// c. A resposta sempre será o número 0 se o número for par.
+
+// d. A resposta sempre será o número 1 se o número for ímpar 
+
+// exercicio 3
+
+// const anos = Number (prompt("Qual sua idade"))  
+
+// const meses = 12 * anos
+
+// console.log("Sua idade em meses é" , meses, "meses")
+
+// const IdadeEmDias = anos * 365
+
+// console.log("Sua idade em dias é" , IdadeEmDias, "dias");
+
+// const IdadeEmHoras = IdadeEmDias * 24
+
+// console.log("Sua idade em horas é" , IdadeEmHoras, "horas")
+
+// Exercicio 4
+
+//const numero1 = Number(prompt("Digite um número"))
+
+// const numero2 = Number(prompt("Digite outro número"))
+
+// const numeroMaior = numero1 > numero2
+
+// console.log("O primeiro numero é maior que segundo?" , numeroMaior)
+
+// const numeroIgual = numero1 === numero2
+
+// console.log("O primeiro numero é igual ao segundo?" , numeroIgual)
+
+// const numeroDivisivel = numero1 % numero2 === 0
+
+// console.log("O primeiro numero é divisível pelo segundo?" , numeroDivisivel);
+
+// const numeroDivisivel2 = numero2 % numero1 === 0
+
+// onsole.log("O segundo numero é divisível pelo primeiro?" , numeroDivisivel2);
+
+
+

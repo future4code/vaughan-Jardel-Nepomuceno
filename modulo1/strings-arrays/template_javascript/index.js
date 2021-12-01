e32ea209e8eba7265af3490a3d2190e8e4617950
jardel <jardelsantos15@gmail.com>
2021-12-01 18:33:48 -0300

    Exercicio feito

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,61 @@
+// 1 .Indique todas as mensagens impressas no console, SEM EXECUTAR o programa.
+
+// Da pra saber a quantidade, inverter a quantidade de letra na lista, e trocar as letras por i.
+
+//2. Leia o código abaixo com atenção 
+
+// Vai pedir pro usuario digitar uma frase.
+
+//Subi num ônibus em Marrocos: essa frase vai ficar todo em Maiuscula, vai trocar o A pelo I, e vai dá a quantidade de letras da frase.
+
+//Exercícios de escrita de código
+
+//1 . Faça um programa que pergunte ao usuário seu nome e seu e-mail. Em seguida, Imprima no console a seguinte mensagem:
+
+//O e-mail emailDoUsuario foi cadastrado com sucesso. Seja bem-vinda(o), nomeDoUsuario!
+
+1. 
+// const seuNome = prompt ("Qual seu nome?")
+// const seuEmail = prompt ("Qual seu e-mail?")
+// const frase = `O e-mail ${seuEmail} foi cadastrado com sucesso. Seja bem-vindo(o), ${seuNome}`
+
+// console.log(frase)
+
+// 2 Faça um programa que contenha um array com 5 das suas comidas favoritas, armazenado em uma variável. Em seguida, siga os passos:
+
+// /const comidaPreferidas = ["banana","lasanha", "macarrao", "feijoada", "arroz"]
+// const escolhaSuaComida = prompt("Qual a sua comida preferida?")
+// const melhorComida = prompt`Qual a sua comida preferida`
+
+// console.log(`Essas são as minhas comidas preferidas:`)
+// console.log(comidaPreferidas[0]);
+// console.log(comidaPreferidas[1]);
+// console.log(comidaPreferidas[2]);
+// console.log(comidaPreferidas[3]);
+// console.log(comidaPreferidas[4]);
+
+
+// 3 Faça um programa, seguindo os passos
+
+//const listaDeTarefas = []
+
+// const pergunta1 = prompt("Cite uma tarefa que você faz no dia a dia")
+// const pergunta2 = prompt("Cite outra tarefa que você faz no dia a dia")
+// const pergunta3 = prompt("Cite mais uma tarefa")
+
+// listaDeTarefas.push(pergunta1,pergunta2,pergunta3)
+
+// const indiceDoUsuario = prompt("digite o índice de uma tarefa que você já realizou")
+
+// const indiceDoUsuario1 = listaDeTarefas.splice('2','2')
+
+// console.log`a tarefa que deseja excluir ${listaDeTarefas}`;
+
+
+
+
+
+
+
+
+

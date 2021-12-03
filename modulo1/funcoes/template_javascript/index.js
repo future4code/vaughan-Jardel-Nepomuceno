684aa09f21df54073638f6db72b5793bf2f57d9d
jardel <jardelsantos15@gmail.com>
2021-12-02 22:06:15 -0300

    exercicio feito

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,82 @@
+// 1 Leia o código abaixo
+
+//A. ele ira retornar o valor dentro da variavel multiplicado por 5, que no caso é 10 e 50 imprimido no console.
+
+//B. Apareceria 2 e 10.
+
+// 2.
+
+// A. Função que pergunta pro usuário do que ele gosta e emite um método includes pra saber se é verdadeiro ou falso.
+
+// B. True, True e True.
+
+// Exercícios de escrita de código.
+
+// 1.
+
+// function mostrarTexto() {
+
+// const nome=("Eu sou Jardel, tenho 25 anos, moro em São Paulo e sou estudante.")
+
+// console.log(nome)
+// }
+
+// mostrarTexto()
+
+// function mostrarInformacoes(nome, idade, cidade, profissao) {
+//   const resultado = `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`;
+
+//   console.log(resultado);
+// }
+
+// mostrarInformacoes("jardel", 25, "Indaiatuba", "desempregada");
+
+// const seuNome = prompt("Qual o seu nome?");
+// const suaIdade = prompt("Qual sua idade?");
+// const SuaCidade = prompt("Qual cidade você mora?");
+// const suaProfissao = prompt("Qual a sua profissão?");
+// const resultado = `Eu sou ${seuNome}, tenho ${suaIdade} anos, moro em ${suaIdade} e sou ${suaProfissao}.`;
+
+// console.log(resultado);
+
+// 2. Escreva as funções explicadas abaixo:
+ 
+//A
+
+// function numeros(n1,n2 ) {
+
+// const resultado = n1 + n2
+
+// return resultado
+
+// console.log(resultado)
+
+// }
+
+// const somaDosNumeros = numeros(10,15)
+
+// console.log(somaDosNumeros); 
+
+//B
+
+// function numeros(n1,n2 ) {
+
+//     const resultado = n1 >= n2
+    
+//     return resultado
+    
+//     console.log(resultado)
+    
+//     }
+
+//     const trueOrFalse = numeros(1+5)
+
+//     console.log(trueOrFalse);
+
+
+
+
+
+
+
+

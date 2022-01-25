06c5d6d7c4efb066cbede0cd2c8146d5626687ec
jardel <jardelsantos15@gmail.com>
2022-01-25 20:36:50 -0300

    exercicio

---
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import axios from 'axios'
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+ <p>OI</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

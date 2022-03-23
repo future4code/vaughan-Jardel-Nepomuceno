cef707958187f97983ef13ca80a5395f0f82eb84
jardel <jardelsantos15@gmail.com>
2022-03-22 21:38:15 -0300

    exercicio feito

---
+++ modulo3/node-package-json/exercicio 1/exercicio1.js
@@ -0,0 +1,14 @@
+// Exercício 1a
+// Utilizando process.argv[index do argumento], sendo os index 0 e 1 fixos do Node.
+
+//Exercício 1b
+
+const nome = process.argv[2];
+const idade = process.argv[3];
+
+console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+
+// Exercício 1c
+
+// const idadeFutura = Number(process.argv[3]) + 7;
+// console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idadeFutura} anos.`)
---
+++ modulo3/node-package-json/exercicio2/index.js
@@ -0,0 +1,19 @@
+
+let operacao = process.argv[2];
+let num1 = Number(process.argv[3]);
+let num2 = Number(process.argv[4]);
+
+switch(operacao) {
+    case "soma":
+        console.log(`Resposta: ${num1 + num2}`)
+        break;
+    case "sub":
+        console.log(`Resposta: ${num1 - num2}`)
+        break;
+    case "mult":
+        console.log(`Resposta: ${num1 * num2}`)
+        break;
+    case "div":
+        console.log(`Resposta: ${num1 / num2}`)
+}
+

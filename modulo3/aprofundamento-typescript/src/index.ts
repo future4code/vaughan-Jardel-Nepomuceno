4cb41e96dcaf0f67d687c10649f2add8c0b2ee7d
jardel <jardelsantos15@gmail.com>
2022-03-24 23:46:46 -0300

    exercicio feito

---
+++ modulo3/aprofundamento-typescript/src/index.ts
@@ -0,0 +1,23 @@
+//exercício 1 
+//a
+
+const minhaString : string = 10
+
+// type number is nor assingnable to type "string"
+// Reclama que a variável não é do tipo string.
+
+//b
+let meuNumero : number | string
+meuNumero = 10, "oiii"
+
+//declarando a variável usando | "ou", para deixar aceitando os dois tipos.
+
+//c
+
+const Person = {
+    nome: string ,
+    idade: number,
+    corFavorita: string
+}
+
+
